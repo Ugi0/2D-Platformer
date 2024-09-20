bbea6904524c2af9f83e4b4ae65d850547c64bfa
Ugi <90718205+Ugi0@users.noreply.github.com>
2024-09-20 16:42:05 +0300

    Make jump more like an arc

--- Assets/Scenes/Move.cs
+++ Assets/Scenes/Move.cs
@@ -26,10 +26,14 @@ public class Move : MonoBehaviour
 
     void MoveObject() {
         horizontal = Input.GetAxis("Horizontal");
-        myRigidbody.velocity = new Vector2(moveSpeed * horizontal, 0);
+        Vector2 newVel = myRigidbody.velocity;
+        newVel.x = moveSpeed * horizontal;
+        //myRigidbody.velocity = new Vector2(moveSpeed * horizontal, 0);
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-            myRigidbody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            newVel.y = jumpHeight;
+            //myRigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
+        myRigidbody.velocity = newVel;
     }
 
     void OnCollisionEnter2D(Collision2D hit)

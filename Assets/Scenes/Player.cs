093a3eaf81f90da6e74213b6081f11a65cb9672c
Ugi <90718205+Ugi0@users.noreply.github.com>
2024-10-06 10:53:02 +0300

    Add a bias to camera direction

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] int moveSpeed;
     [SerializeField] int jumpHeight;
@@ -10,28 +10,32 @@ public class Move : MonoBehaviour
     float horizontal;
 
     bool isGrounded;
+    bool isFacingRight;
 
     Rigidbody2D myRigidbody;
+    Transform player;
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveObject();
+        MoveControls();
     }
 
-    void MoveObject() {
+    void MoveControls() {
         horizontal = Input.GetAxis("Horizontal");
+        if ((horizontal < 0 && isFacingRight) || (horizontal > 0 && !isFacingRight)) {
+            Turn();
+        }
         Vector2 newVel = myRigidbody.velocity;
         newVel.x = moveSpeed * horizontal;
-        //myRigidbody.velocity = new Vector2(moveSpeed * horizontal, 0);
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
             newVel.y = jumpHeight;
-            //myRigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
         myRigidbody.velocity = newVel;
     }
@@ -46,6 +50,19 @@ public class Move : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D hit)
     {
-            isGrounded = false;
+        isGrounded = false;
     }
+
+    void Turn() {
+        if (isFacingRight) {
+            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = !isFacingRight;
+        } else {
+            Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = !isFacingRight;
+        }
+    }
+
 }
